429ee236af98e27be7cca9d7bdc74d8465742f32
Aaron Shappell <aaronshappell@gmail.com>
2021-05-17 20:18:58 -0700

    Start to setup trace loading/testing

--- sim.cpp
+++ sim.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <fstream>
+#include <string>
 #include <verilated.h>
 
 #include "Vtop.h"
@@ -6,26 +9,47 @@ int main(int argc, char** argv, char** env) {
     // Prevent unused variable warnings
     if (false && argc && argv && env) {}
 
-    // Construct verilated model
-    Vtop* top = new Vtop;
-
-    // Set input signals
-    top->clk_i = 0;
-    top->reset_i = 1;
-    top->eval();
-    top->reset_i = 0;
-
     // Simulate
-    for(int i = 0; i < 10; i++) {
-        top->clk_i = 1;
-        top->eval();
+    for(int i = 0; i < 5; i++) {
+        // Construct verilated model
+        Vtop* top = new Vtop;
+
+        // Statistics
+        float mpki = 0;
+
+        // Set input signals
         top->clk_i = 0;
+        top->reset_i = 1;
         top->eval();
-        VL_PRINTF("count: %d\n", top->count);
+        top->reset_i = 0;
+
+        std::string line;
+        std::string filename = "traces/trace" + std::to_string(i);
+        std::ifstream trace(filename);
+        if(trace.is_open()) {
+            while(getline(trace, line)) {
+                top->clk_i = 1;
+                top->eval();
+                top->clk_i = 0;
+                top->eval();
+                // Test predictor
+                // TODO
+            }
+            trace.close();
+        } else {
+            VL_PRINTF("ERROR: unable to open file: %s\n", filename.c_str());
+        }
+
+        // Calculate MPKI
+        // TODO
+
+        // Print results
+        VL_PRINTF("%s mpki: %f\n", filename.c_str(), mpki);
+
+        // Cleanup
+        top->final();
+        delete top;
     }
 
-    // Cleanup
-    top->final();
-    delete top;
     return 0;
 }
