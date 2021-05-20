301755fa3e519ca9cb887ef167655a613a66de20
Aaron Shappell <aaronshappell@gmail.com>
2021-05-19 22:53:26 -0700

    Have makefile search for src/v dir, add simple always taken predictor

--- src/v/always_predictor.sv
+++ src/v/always_predictor.sv
@@ -1,10 +1,14 @@
+/*
+Simple always taken branch predictor
+*/
+
 `include "common_defines.svh"
 
-module top
+module always_predictor
     (
         input logic clk_i,
         input logic reset_i,
         output logic prediction
     );
-    always_predictor p (.*);
+    assign prediction = `BR_TAKEN;
 endmodule
---
+++ tb/tage_component.cpp
@@ -0,0 +1,15 @@
+#include <verilated.h>
+
+#include "Vtage_component.h"
+
+int main(int argc, char** argv, char** env) {
+    // Prevent unused variable warnings
+    if (false && argc && argv && env) {}
+
+    // Construct verilated model
+    Vtage_component* component = new Vtage_component;
+
+    
+
+    return 0;
+}
