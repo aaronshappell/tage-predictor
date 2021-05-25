21fda51d8e9fe5a33a9682abf786bf31f39bba02
Aaron Shappell <aaronshappell@gmail.com>
2021-05-24 21:22:57 -0700

    Remove temp python script, fix top lint warning

--- src/v/top.sv
+++ src/v/top.sv
@@ -2,15 +2,15 @@
 
 module top
     (
-        input logic clk_i,
         /* verilator lint_off UNUSED */
+        input logic clk_i,
         input logic br_result_i, update_en_i, correct_i,
         input logic [31:0] idx_i,
         /* verilator lint_on UNUSED */
         output logic prediction_o
     );
     /* verilator lint_off WIDTH */
-    // assign prediction_o = `BR_TAKEN;
+    //assign prediction_o = `BR_TAKEN;
     //bht b (.*);
     tage_predictor tp (.*);
     /* verilator lint_on WIDTH */
