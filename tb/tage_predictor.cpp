7d5795b3d023351de75dd2b0cc0d67b3ed6be5dc
Aaron Shappell <aaronshappell@gmail.com>
2021-05-25 21:43:52 -0700

    Only count conditional branches, calc mpki

--- tb/tage_predictor.cpp
+++ tb/tage_predictor.cpp
@@ -5,18 +5,19 @@
 
 #include "Vtage_predictor.h"
 
-#define NUM_TRACES 13
+#define NUM_TRACES 20
 
 int main(int argc, char** argv, char** env) {
     // Prevent unused variable warnings
     if (false && argc && argv && env) {}
 
+    // Construct verilated model
+    Vtage_predictor* tp = new Vtage_predictor;
+
     // Simulate traces
     for(int i = 0; i < NUM_TRACES; i++) {
-        // Construct verilated model
-        Vtage_predictor* tp = new Vtage_predictor;
-
         // Statistics
+        int num_instr = 0;
         int num_branches = 0;
         int num_mispred = 0;
         int prev_branchresult = 0;
@@ -29,12 +30,17 @@ int main(int argc, char** argv, char** env) {
 
         std::string address;
         std::string branchresult;
+        std::string conditional;
         std::ifstream address_file("traces/addresses" + std::to_string(i));
         std::ifstream branchresult_file("traces/branchresults" + std::to_string(i));
+        std::ifstream conditional_file("traces/conditionals" + std::to_string(i));
         if(address_file.is_open() && branchresult_file.is_open()) {
             while(getline(address_file, address)) {
                 getline(branchresult_file, branchresult);
-                num_branches++;
+                getline(conditional_file, conditional);
+                if(std::stoi(conditional)) {
+                    num_branches++;
+                }
 
                 tp->br_result_i = prev_branchresult;
                 tp->correct_i = tp->prediction_o == prev_branchresult;
@@ -43,7 +49,7 @@ int main(int argc, char** argv, char** env) {
                 tp->eval();
                 tp->clk_i = 0;
                 tp->eval();
-                if (tp->prediction_o != std::stoi(branchresult)) {
+                if (tp->prediction_o != std::stoi(branchresult) && std::stoi(conditional)) {
                     num_mispred++;
 
                     switch (tp->tage_predictor__DOT__providers) {
@@ -69,12 +75,14 @@ int main(int argc, char** argv, char** env) {
             }
             address_file.close();
             branchresult_file.close();
+            conditional_file.close();
         } else {
             VL_PRINTF("ERROR: unable to open trace files\n");
         }
 
         // Calculate mispred_rate
         float mispred_rate = (float) num_mispred / (float) num_branches;
+        float mpki = (float) num_mispred / ((float) num_instr / 1000);
 
         float T0_mpr = (float) T0_mispred / (float) T0;
         float T1_mpr = (float) T1_mispred / (float) T1;
@@ -84,17 +92,18 @@ int main(int argc, char** argv, char** env) {
 
         // Print results
         VL_PRINTF("-- Trace %d Results -------------\n", i);
-        VL_PRINTF("  Branches: %d\n", num_branches);
+        VL_PRINTF("  Conditional Branches: %d\n", num_branches);
         VL_PRINTF("  Mispredictions: %d\n", num_mispred);
         VL_PRINTF("  Misprediction Rate: %f\n", mispred_rate);
+        VL_PRINTF("  MPKI: %f\n\n", mpki);
         VL_PRINTF("T0 preds: %d, T1 preds: %d, T2 preds: %d, T3 preds: %d, T4 preds: %d\n", T0, T1, T2, T3, T4);
         VL_PRINTF("T0_mp: %d, T1_mp: %d, T2_mp: %d, T3_mp: %d, T4_mp: %d\n", T0_mispred, T1_mispred, T2_mispred, T3_mispred, T4_mispred);
         VL_PRINTF("T0_mpr: %f, T1_mpr: %f, T2_mpr: %f, T3_mpr: %f, T4_mpr: %f\n\n", T0_mpr, T1_mpr, T2_mpr, T3_mpr, T4_mpr);
-
-        // Cleanup
-        tp->final();
-        delete tp;
     }
+
+    // Cleanup
+    tp->final();
+    delete tp;
 
     return 0;
 }
--- tb/top.cpp
+++ tb/top.cpp
@@ -5,18 +5,19 @@
 
 #include "Vtop.h"
 
-#define NUM_TRACES 13
+#define NUM_TRACES 20
 
 int main(int argc, char** argv, char** env) {
     // Prevent unused variable warnings
     if (false && argc && argv && env) {}
 
+    // Construct verilated model
+    Vtop* top = new Vtop;
+
     // Simulate traces
     for(int i = 0; i < NUM_TRACES; i++) {
-        // Construct verilated model
-        Vtop* top = new Vtop;
-
         // Statistics
+        int num_instr = 0;
         int num_branches = 0;
         int num_mispred = 0;
         int prev_branchresult = 0;
@@ -25,12 +26,19 @@ int main(int argc, char** argv, char** env) {
 
         std::string address;
         std::string branchresult;
+        std::string conditional;
         std::ifstream address_file("traces/addresses" + std::to_string(i));
         std::ifstream branchresult_file("traces/branchresults" + std::to_string(i));
-        if(address_file.is_open() && branchresult_file.is_open()) {
+        std::ifstream conditional_file("traces/conditionals" + std::to_string(i));
+        if(address_file.is_open() && branchresult_file.is_open() && conditional_file.is_open()) {
+            getline(address_file, address);
+            num_instr = std::stoi(address);
             while(getline(address_file, address)) {
                 getline(branchresult_file, branchresult);
-                num_branches++;
+                getline(conditional_file, conditional);
+                if(std::stoi(conditional)) {
+                    num_branches++;
+                }
 
                 top->br_result_i = prev_branchresult;
                 top->correct_i = top->prediction_o == prev_branchresult;
@@ -39,30 +47,33 @@ int main(int argc, char** argv, char** env) {
                 top->eval();
                 top->clk_i = 0;
                 top->eval();
-                if (top->prediction_o != std::stoi(branchresult)) {
+                if (top->prediction_o != std::stoi(branchresult) && std::stoi(conditional)) {
                     num_mispred++;
                 }
                 prev_branchresult = std::stoi(branchresult);
             }
             address_file.close();
             branchresult_file.close();
+            conditional_file.close();
         } else {
             VL_PRINTF("ERROR: unable to open trace files\n");
         }
 
         // Calculate mispred_rate
         float mispred_rate = (float) num_mispred / (float) num_branches;
+        float mpki = (float) num_mispred / ((float) num_instr / 1000);
 
         // Print results
         VL_PRINTF("-- Trace %d Results -------------\n", i);
-        VL_PRINTF("  Branches: %d\n", num_branches);
+        VL_PRINTF("  Conditional Branches: %d\n", num_branches);
         VL_PRINTF("  Mispredictions: %d\n", num_mispred);
-        VL_PRINTF("  Misprediction Rate: %f\n\n", mispred_rate);
-
-        // Cleanup
-        top->final();
-        delete top;
+        VL_PRINTF("  Misprediction Rate: %f\n", mispred_rate);
+        VL_PRINTF("  MPKI: %f\n\n", mpki);
     }
+
+    // Cleanup
+    top->final();
+    delete top;
 
     return 0;
 }
